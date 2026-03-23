679759a6a786c2327b3acb1a71f5f45047451ad7
Mike Samuel <mikesamuel2814@gmail.com>
2026-03-23 15:44:39 +0700

    asdferasdf

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,15 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["prime.primeclinic24.com"],
     hmr: {
       overlay: false,
     },
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: ["prime.primeclinic24.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
